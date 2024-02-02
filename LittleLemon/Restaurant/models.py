25a5d2031c86a05bfdd0a0ad0b3ed2e399f6da70
nicholaswfowler <nicholas.w.fowler@gmail.com>
2024-02-02 01:29:20 -0600

    booking and menu models created

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    title = models.CharField(max_length = 255)
+    guests =  models.IntegerField()
+    date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title + ': ' + str(self.guests) + ' Guests'
+
+class Menu(models.Model):
+    title = models.CharField(max_length = 255)
+    price = models.DecimalField(decimal_places = 2, max_digits = 10)
+    inventory = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
