e03d1c7c8bf22bfc30d8108eb8d446b704cc619d
nicholaswfowler <153998644+nicholaswfowler@users.noreply.github.com>
2024-02-02 23:58:02 -0600

    Update test_views.py

--- LittleLemon/tests/test_views.py
+++ LittleLemon/tests/test_views.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
 from Restaurant.models import Menu
+from Restaurant.serializers import MenuSerializer
 from django.core import serializers
+
 class MenuViewTest(TestCase):
     def setUp(self):
-        Menu.objects.create(title="Cheese Pizza", price=12, inventory=50)
-        Menu.objects.create(title="Pepperoni Pizza", price=12, inventory=50)
-        Menu.objects.create(title="Veggie Pizza", price=12, inventory=50)
+        self.client = APIClient()
+        self.menu_item1 = Menu.objects.create(title="Burger", price=10.99, inventory=50)
+        self.menu_item2 = Menu.objects.create(title="Pizza", price=12.99, inventory=40)
 
     def test_getall(self):
-        data = serializers.serialize('json',Menu.objects.all())
-        self.assertEqual(data, data)
+        response = self.client.get('/restaurant/menu/items/')
+        menu_items = Menu.objects.all()
+        serializer = MenuSerializer(menu_items, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, 200)
