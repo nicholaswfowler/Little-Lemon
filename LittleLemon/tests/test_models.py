f564b8c5025ef379d2c094b54bab143c907e5af3
nicholaswfowler <153998644+nicholaswfowler@users.noreply.github.com>
2024-02-02 23:58:49 -0600

    Update test_models.py

--- LittleLemon/tests/test_models.py
+++ LittleLemon/tests/test_models.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from Restaurant.models import Menu
 
-class MenuTest(TestCase):
+
+class MenuItemTest(TestCase):
     def test_get_item(self):
         item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-        item_str = f'{item.title}: {str(item.price)}'
-        self.assertEqual(item_str, "IceCream: 80")
+        self.assertEqual(str(item), "IceCream: 80")
